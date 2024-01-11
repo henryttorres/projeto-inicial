3c64d1343fa06e5daf0e78165465193efe8d0040
Henry Torres <henry.torres.batista@gmail.com>
2024-01-11 02:09:20 -0300

    projeto inicial

---
+++ exemploscript.js
@@ -0,0 +1,8 @@
+let nome = prompt('Qual o seu nome?');
+
+let idade = prompt('Qual a sua idade?');
+alert('Seu nome é ' + nome + ' e sua idade é: ' +idade);
+var var1 = 1,
+    var2 = 2,
+    var3 = 3;
+    console.log(var3);
